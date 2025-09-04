2089720a88219fc53b24a94762840670b164429e
LinThitHtwe <linthit2745@gmail.com>
2025-09-04 12:46:44 +0800

    [init] project & [add] register_user

---
+++ apiappTest/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiapptestConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apiappTest'
---
+++ apiappTest/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = get_user_model()
+        fields = ["id", "username", "email", "password", "first_name", "last_name" ]
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+
+        email = validated_data.get('email')
+        username = validated_data.get('username')
+        password = validated_data.get('password')
+        first_name = validated_data.get('first_name')
+        last_name = validated_data.get('last_name')
+
+        user = get_user_model()
+        new_user = user.objects.create_user(
+            email=email,
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+        )
+        new_user.set_password(password)
+        new_user.save()
+        return new_user
